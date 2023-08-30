cd6d49efba04bc922c258d6f17962a4041a8cb3a
Dashrath <sandip9028162859@gmail.com>
2023-08-30 06:40:48 +0530

    url change

--- HomeBackEnd/index.js
+++ HomeBackEnd/index.js
@@ -14,7 +14,7 @@ const adminSchema = require("./Schema/adminSchema");
 // https://homerent.netlify.app
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    origin:"https://homerent.netlify.app",
     credentials: true,
   })
 );  
